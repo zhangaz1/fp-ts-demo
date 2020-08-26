1d403345a61b52c1260b167e99f9939120c9d29b
zhangaz1 <zhangaz1@hotmail.com>
2020-08-26 15:14:01 +0800

    use debug

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,13 @@
-describe('index', () => {
-	test('first', () => {
+import createDebug from 'debug';
+import * as fp from 'fp-ts';
 
+const currentFile = __filename.replace(process.env.PWD, '');
+const debug = createDebug(`test:${currentFile}`);
+
+describe(`${currentFile}`, () => {
+	test('log fp-ts', () => {
+		debug('%s', fp);
+		debug('%s', process.env.PWD);
+		debug('alt: %s', fp.alt);
 	});
 });
