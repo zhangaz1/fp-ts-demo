8f44f465d8c67eef525f85ad09cb1090055ace18
zhangaz1 <zhangaz1@hotmail.com>
2020-08-30 20:18:39 +0800

    test random read file

--- src/ioEither.test.ts
+++ src/ioEither.test.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import createDebug from 'debug';
+import { randomInt } from 'fp-ts/lib/Random';
 import {
 	isRight,
 	isLeft,
@@ -8,19 +9,23 @@ import {
 import {
 	IOEither,
 	tryCatch,
+	ioEither,
+	rightIO,
+	map,
 } from 'fp-ts/lib/IOEither';
 
 const currentFile = __filename.replace(process.env.PWD, '');
 const debug = createDebug(`test:${currentFile}`);
 
 describe(`${currentFile}`, () => {
+	function readFileSync(path: string): IOEither<Error, string> {
+		return tryCatch(
+			() => fs.readFileSync(path, 'utf8'),
+			toError
+		);
+	}
+
 	describe('IOEither', () => {
-		function readFileSync(path: string): IOEither<Error, string> {
-			return tryCatch(
-				() => fs.readFileSync(path, 'utf8'),
-				toError
-			);
-		}
 
 		test('right', () => {
 			expect(
@@ -35,6 +40,26 @@ describe(`${currentFile}`, () => {
 				isLeft(
 					readFileSync('./xx.abc')()
 				)
+			).toBeTruthy();
+		});
+	});
+
+	describe('random file', () => {
+		const files = [
+			`${__dirname}/io.test.ts`,
+			`${__dirname}/either.test.ts`,
+			`${__dirname}/ioEither.test.ts`,
+		];
+
+		const randomReadFile = ioEither.chain(
+			map((n: number) => files[n])
+				(rightIO(randomInt(0, 2))),
+			readFileSync
+		);
+
+		test('random read file', () => {
+			expect(
+				isRight(randomReadFile())
 			).toBeTruthy();
 		});
 	});
