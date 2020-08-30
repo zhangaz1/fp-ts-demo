63cebae313f1d587c0cda833d7dc907cc64df9de
zhangaz1 <zhangaz1@hotmail.com>
2020-08-30 19:27:42 +0800

    use toError

--- src/ioEither.test.ts
+++ src/ioEither.test.ts
@@ -18,7 +18,7 @@ describe(`${currentFile}`, () => {
 		function readFileSync(path: string): IOEither<Error, string> {
 			return tryCatch(
 				() => fs.readFileSync(path, 'utf8'),
-				reason => new Error(String(reason))
+				toError
 			);
 		}
 
