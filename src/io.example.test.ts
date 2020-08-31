2d091aaabf32407c629a0df391eb9b9607690c29
zhangaz1 <zhangaz1@hotmail.com>
2020-09-01 07:53:01 +0800

    add withLogging

--- src/io.example.test.ts
+++ src/io.example.test.ts
@@ -26,4 +26,35 @@ describe(`${currentFile}`, () => {
 			time(() => 5)()
 		).toBe(5);
 	});
+
+	const time2 = <A>(ma: fp.io.IO<A>): fp.io.IO<[A, number]> =>
+		fp.io.io.chain(
+			fp.date.now,
+			start => fp.io.io.chain(
+				ma,
+				a => fp.io.io.chain(
+					fp.date.now,
+					end =>
+						() => [a, end - start]
+				)
+			)
+		);
+
+	const withLogging = <A>(ma: fp.io.IO<A>): fp.io.IO<A> =>
+		fp.io.io.chain(
+			time2(ma),
+			([a, millis]) => fp.io.io.map(
+				fp.console.log(`Result: ${a}, Elapsed: ${millis}`),
+				() => a
+			)
+		);
+
+	const fib = (n: number): number =>
+		n <= 1
+			? 1
+			: fib(n - 1) + fib(n - 2);
+
+	const program = fp.io.map(withLogging)(() => () => fib(35));
+
+	program()();
 });
