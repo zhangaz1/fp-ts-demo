664171d2dba357d892d5cdafe4fb8a965a95e878
zhangaz1 <zhangaz1@hotmail.com>
2020-09-04 16:01:14 +0800

    add monoid test

--- src/fc.test.ts
+++ src/fc.test.ts
@@ -1,18 +1,37 @@
 import * as fp from 'fp-ts';
 import * as fc from 'fast-check';
+import { mapLeft } from 'fp-ts/lib/Either';
 
 const currentFile = __filename.replace(process.env.PWD, '');
 
 describe(`${currentFile}`, () => {
 	const S: fp.semigroup.Semigroup<string> = {
-		concat: (x, y) => x + ' ' + y,
+		concat: (x, y) => x === ''
+			? y
+			: y === ''
+				? x
+				: x + ' ' + y,
 	};
+	const arb: fc.Arbitrary<string> = fc.string();
 
 	const associativity = (x: string, y: string, z: string) =>
 		S.concat(S.concat(x, y), z) === S.concat(x, S.concat(y, z));
 
-	test('0', () => {
-		const arb: fc.Arbitrary<string> = fc.string();
+	test('associativity', () => {
 		fc.assert(fc.property(arb, arb, arb, associativity));
 	});
+
+	const M: fp.monoid.Monoid<string> = {
+		...S,
+		empty: '',
+	};
+
+	const rightIdentity = (x: string) => M.concat(x, M.empty) === x;
+	const leftIdentity = (x: string) => M.concat(M.empty, x) === x;
+
+	test('monoid instance should be lawful', () => {
+		fc.assert(fc.property(arb, rightIdentity));
+		fc.assert(fc.property(arb, leftIdentity));
+		expect(M.concat('', M.empty)).toBe('');
+	})
 });
